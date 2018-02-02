411f69c4a877fa60c0d7400b87ed8c957faee934
Fin Kingma <fkingma@xebia.com>
2018-02-02 15:32:02 +0100

    added information hiding

--- src/modules/Calculator.js
+++ src/modules/Calculator.js
@@ -1,7 +1,9 @@
 var Calculator = {}
 
+var KEY = ' transformed'
+
 Calculator.transform = (value) => {
-  return value + value
+  return value + KEY
 }
 
 module.exports = Calculator
--- test/unit/specs/Calculator.spec.js
+++ test/unit/specs/Calculator.spec.js
@@ -2,6 +2,6 @@ import { transform } from '@/modules/Calculator'
 
 describe('Calculator', () => {
   it('should transform data', () => {
-    expect(transform(1)).toEqual(2)
+    expect(transform(1)).toEqual('1 transformed')
   })
 })
